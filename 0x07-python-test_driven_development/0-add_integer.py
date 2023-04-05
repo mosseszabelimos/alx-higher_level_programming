5f7051ac272475a01746d9360a34b0862fcfd17c
Abel <abeldawit.h@gmail.com>
2023-04-05 09:46:18 +0300

    more tasks addes

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-# 0-add_integer.py
-"""Defines an integer addition function."""
+"""Module built for Python 0x07 task 0.
+"""
 
 
 def add_integer(a, b=98):
-    """Return the integer addition of a and b.
-
-    Float arguments are typecasted to ints before addition is performed.
-
-    Raises:
-        TypeError: If either of a or b is a non-integer and non-float.
+    """Function that adds two integers.
+    Args:
+        a ((int, (float)): first arg to add to sum.
+        b ((int, (float)): second arg to add to sum. Defaults to 98.
+    Returns: sum of both values.
     """
-    if ((not isinstance(a, int) and not isinstance(a, float))):
-        raise TypeError("a must be an integer")
-    if ((not isinstance(b, int) and not isinstance(b, float))):
-        raise TypeError("b must be an integer")
-    return (int(a) + int(b))
+    if type(a) is float:
+        a = int(a)
+    elif type(a) is not int:
+        raise TypeError('a must be an integer')
+
+    if type(b) is float:
+        b = int(b)
+    elif type(b) is not int:
+        raise TypeError('b must be an integer')
+
+    return a + b
