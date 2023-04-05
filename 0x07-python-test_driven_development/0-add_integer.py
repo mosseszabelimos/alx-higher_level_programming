00109062216b9731af83bf64688edd9d7a642b63
Abel <abeldawit.h@gmail.com>
2023-04-05 10:00:50 +0300

    updating test folder

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
-"""Module built for Python 0x07 task 0.
-"""
+add_integer = __import__('0-add_integer').add_integer
 
+print(add_integer(1, 2))
+print(add_integer(100, -2))
+print(add_integer(2))
+print(add_integer(100.3, -2))
+try:
+    print(add_integer(4, "School"))
+except Exception as e:
+    print(e)
+try:
+    print(add_integer(None))
+except Exception as e:
+    print(e)
 
-def add_integer(a, b=98):
-    """Function that adds two integers.
-    Args:
-        a ((int, (float)): first arg to add to sum.
-        b ((int, (float)): second arg to add to sum. Defaults to 98.
-    Returns: sum of both values.
-    """
-    if type(a) is float:
-        a = int(a)
-    elif type(a) is not int:
-        raise TypeError('a must be an integer')
-
-    if type(b) is float:
-        b = int(b)
-    elif type(b) is not int:
-        raise TypeError('b must be an integer')
-
-    return a + b
