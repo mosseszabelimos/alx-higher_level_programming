cd6aa8e538f25cf1ea898abd3a99e567abfe3932
Abel <abeldawit.h@gmail.com>
2023-04-10 05:23:07 -0700

    fixxing error for task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-""" Rectangle subclass module """
+""" Rectangle subclass """
+
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """ Rectangle class inherits from BaseGeometry """
+    """ Rectangle that inherits from BaseGeometry """
     def __init__(self, width, height):
         BaseGeometry.integer_validator(self, "height", height)
         self.__height__ = height
         BaseGeometry.integer_validator(self, "width", width)
         self.__width__ = width
+
+    def area(self):
+        return self.__width__ * self.__height__
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width__, self.__height__)
