55ac8aac640b81192c1974594f8ff6f7ed459ea3
Abel <abeldawit.h@gmail.com>
2023-05-17 11:29:52 +0000

    task 9 error fixed

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
---a script that lists all cities contained in the database hbtn_0d_usa
+-- a script that lists all cities contained in the database hbtn_0d_usa
 SELECT a.id AS id, a.name AS name, b.name AS name 
 FROM cities a 
 INNER JOIN states b 
