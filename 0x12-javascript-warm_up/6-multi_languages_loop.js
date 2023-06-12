f8c08b0f08ccfc4a40ed9d92eac2c63b54e00b9b
Abel <abeldawit.h@gmail.com>
2023-06-12 02:29:28 -0700

    error fixing task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-// A script that prints 3 lines: (like 1-multi_languages.js) 
-// using an array of string and a loop
-
+// A script that prints 3 lines:
 const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-
 for (let i = 0; i < 3; i++) {
   console.log(langs[i]);
 }
