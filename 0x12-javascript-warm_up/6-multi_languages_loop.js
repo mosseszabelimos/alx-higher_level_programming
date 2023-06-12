e28d9bf7e2defe75f4150c0777170be368c16166
Abel <abeldawit.h@gmail.com>
2023-06-12 02:08:37 -0700

    correcting error for task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-// A script that prints 3 lines: (like 1-multi_languages.js) using an array of string and a loop
+// A script that prints 3 lines: (like 1-multi_languages.js) 
+// using an array of string and a loop
 
 const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
