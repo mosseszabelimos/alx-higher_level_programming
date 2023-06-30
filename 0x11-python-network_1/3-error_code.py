e97b3f265d8ea7183e567f2136518484bfb4deb7
Abel <abeldawit.h@gmail.com>
2023-06-30 11:27:49 +0000

    task 3 error fixed

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
+"""script for testing status of web pages
 """
-script that takes in a URL, sends a request to the URL and displays the
-body of the response (decoded in utf-8).
-"""
-import urllib.request
-import urllib.error
-import sys
-
-
 if __name__ == "__main__":
+    import urllib.request
+    import urllib.error
+    import sys
     url = sys.argv[1]
     try:
-        with urllib.request.urlopen(url) as response:
-            response_text = response.read().decode("utf-8")
-            print(response_text)
+        with urllib.request.urlopen(url) as resp:
+            print(resp.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
-	print("Error code: {}".format(e.code))
+        print("Error code: {}".format(e.code))
