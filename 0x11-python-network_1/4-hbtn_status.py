5eb3c7afb04892136406b9cc516a090c4b10cf21
Abel <abeldawit.h@gmail.com>
2023-06-30 11:53:03 +0000

    fixing error for task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""Write a Python script for testing status of web pages
 """
+write a Python script that fetches https://alx-intranet.hbtn.io/status.
+"""
+import requests
+
+
 if __name__ == "__main__":
-    import requests
-    url = "https://intranet.hbtn.io/status"
+    url = "https://alx-intranet.hbtn.io/status"
     response = requests.get(url)
     content = response.text
-    print_str = '''Body response:
-\t- type: {}
-\t- content: {}'''.format(type(content), content)
-    print(print_str)
+    print('Body response:')
+    print('\t- type: {}'.format(type(content)))
+    print('\t- content: {}'.format(content))
